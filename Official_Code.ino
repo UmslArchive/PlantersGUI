b85a4a9b588f09dd89164a00180185291a5de588
Jet Huang <35970955+JetSaysHi@users.noreply.github.com>
2019-05-01 14:49:46 -0500

    Update whole file

--- Official_Code.ino
+++ Official_Code.ino
@@ -5,11 +5,10 @@ const int chipSelect = 4;                         //Selected pin for SD card rea
 int motorPin = A0;                                //Pin that turns on the motor.
 int blinkPin = 13;                                //Pin that turns on the LED.
 int soilPin = A1;                                 //Declare a variable for the soil moisture sensor.
+int soilPower = 7;                                //Variable for Soil moisture Power pin.
 
-int soilPower = 7;                                //Variable for Soil moisture Power.
-
-int watertime = 5;                                //How long to water in seconds.
-int waittime = 1;                                //How long to wait between watering in minutes.
+int watertime = 30;                               //How long to water in seconds (100mL/minute).
+int waittime = 1;                                 //How long to wait between watering in minutes.
 
 int sensorValue = 0;                              //Value for storing moisture value.
 int moistureLevelHolder = 0;                      //Value hold for calculation.
@@ -23,13 +22,14 @@ void setup()
   Serial.begin(9600);                             // open serial over USB.
   while (!Serial) 
   {
-    ; //Wait for serial port to connect. Needed for native USB port only
+    ;                                             //Wait for serial port to connect. Needed for native USB port only.
   }
 
   Serial.print("Initializing SD card...");        //See if the card is present and can be initialized.
   
   if (!SD.begin(chipSelect))                      //Error handling for SD card.
   {
+    Serial.println("Card failed, or not present");         
     while (1);
   }
   Serial.println("card initialized.");
@@ -44,7 +44,7 @@ void setup()
 void loop() 
 {  
   String moistureLog = "";                        //Make a string for assembling the data to log.
-  String averageMoistureLog = "";
+  String averageMoistureLog = "";                 //Make a string for average moisture logging.
   int sensor = analogRead(soilPin);               //Read Moisture sensors and append to the string.
   moistureLog += String(sensor);
   averageMoistureLog += String(averageMoistureLevel);
@@ -52,9 +52,9 @@ void loop()
   //Open the file. note that only one file can be open at a time,
   //So you have to close this one before opening another.
   File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
+  
   //If the file is available, write to it.
-  if(dataFile)                                  //Output to datalog.txt.
+  if(dataFile)                                    //Output to datalog.txt.
   {
     dataFile.print("Soil Moisture: " + moistureLog + " | Average value: " + averageMoistureLog);
     dataFile.println();
@@ -62,29 +62,30 @@ void loop()
   }
   else 
   {
-    Serial.println("error opening datalog.txt");  //If the file isn't open, pop up an error.
+    Serial.println("Error opening: datalog.txt");  //If the file isn't open, pop up an error.
   }
 
   //This three second timefrme is used so you can test the sensor and see it change in real-time.
   //For in-plant applications, you will want to take readings much less frequently.
-  delay(2000);                                  //take a reading every two second
+  delay(2000);                                    //Take a reading every two second.
   
-  Serial.print("Soil Moisture = ");
+  Serial.print("Soil Moisture: ");
   Serial.println(readSoil());
-  //"Average moisture level: " + averageMoistureLevel
+
+  delay(1000);                                    //Delay for one second.
   
-  if(sensorValue < 1000)                        //Change value on simple testing.
+  if(sensorValue < 1000)                          //Change value on simple testing.
   {
-    digitalWrite(soilPower, LOW); //turn D7 "Off"
+    digitalWrite(soilPower, LOW);                 //Turn D7 "Off".
     Serial.print("Pumping water \n");
 
-    digitalWrite(motorPin, HIGH);               //Turn on the motor.
-    digitalWrite(blinkPin, HIGH);               //Turn on the LED.
-    delay(watertime * 1000);                    //Multiply by 1000 to translate seconds to milliseconds.
+    digitalWrite(motorPin, HIGH);                 //Turn on the motor.
+    digitalWrite(blinkPin, HIGH);                 //Turn on the LED.
+    delay(watertime * 1000);                      //Multiply by 1000 to translate seconds to milliseconds.
 
-    digitalWrite(motorPin, LOW);                //Turn off the motor.
-    digitalWrite(blinkPin, LOW);                //Turn off the LED.
-    delay(waittime * 1000);                     //Multiply by 60000 to translate minutes to milliseconds.
+    digitalWrite(motorPin, LOW);                  //Turn off the motor.
+    digitalWrite(blinkPin, LOW);                  //Turn off the LED.
+    delay(waittime * 1000);                       //Multiply by 60000 to translate minutes to milliseconds.
 
     Serial.print("Pumping stopped \n");
   }
@@ -92,14 +93,14 @@ void loop()
 
 int readSoil()
 {
-  digitalWrite(soilPower, HIGH);                  //turn D7 "On".
-  delay(100);                                     //wait 100 milliseconds.
-  sensorValue = analogRead(soilPin);              //Read the SIG value form sensor.
+  digitalWrite(soilPower, HIGH);                    //turn D7 "On".
+  delay(100);                                       //wait 100 milliseconds.
+  sensorValue = analogRead(soilPin);                //Read the SIG value form sensor.
 
   //Calculate our average moisture level.
   moistureLevelHolder = averageMoistureLevel + sensorValue; 
   moistureIntervals = moistureIntervals + 1;
   averageMoistureLevel = moistureLevelHolder / moistureIntervals;
   
-  return sensorValue;                             //send current moisture value back to loop for SD readings.
+  return sensorValue;                               //Send current moisture value back to loop for SD readings.
 }
