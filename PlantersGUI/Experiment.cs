ff15c3f46e4b446daaa1ddd74bac5817f4f4a7cd
Colby Ackerman <colbyackerman@gmail.com>
2019-05-03 11:52:08 -0500

    Added class Experiment to Experiment.cs

---
+++ PlantersGUI/Experiment.cs
@@ -0,0 +1,18 @@
+﻿namespace PlantersGUI
+{
+    public static class Experiment
+    {
+        public static Table[] tables;
+
+        //Experiment initialization
+        static Experiment()
+        {
+
+        }
+         
+        //Save currently open experiment to file.
+        static void save()
+        {
+        }
+    }
+}
