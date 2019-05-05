bffe3796b2c255c2dcb8ff487a23cd9493df4106
Colby Ackerman <colbyackerman@gmail.com>
2019-05-05 12:27:26 -0500

    device monitor group box text

--- PlantersGUI/ContinueForm.Designer.cs
+++ PlantersGUI/ContinueForm.Designer.cs
@@ -92,7 +92,7 @@
             this.deviceMonitorGroupBox.Size = new System.Drawing.Size(349, 327);
             this.deviceMonitorGroupBox.TabIndex = 6;
             this.deviceMonitorGroupBox.TabStop = false;
-            this.deviceMonitorGroupBox.Text = "Devices Monitor";
+            this.deviceMonitorGroupBox.Text = "Device Monitor";
             this.deviceMonitorGroupBox.Enter += new System.EventHandler(this.DeviceMonitorGroupBox_Enter);
             // 
             // deviceStatusLabel
